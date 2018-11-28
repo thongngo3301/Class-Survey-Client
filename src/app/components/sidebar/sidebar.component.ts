1a2cc9271b840e607ba47222d43f9f95b25382bf
ThongND <thongngo3301@gmail.com>
2018-11-28 11:49:23 +0700

    init new client

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+const adminRoutes = [
+    { path: '/home', title: 'Home', icon: 'design_app', class: '' },
+    { path: '/icons', title: 'Icons', icon: 'education_atom', class: '' },
+    { path: '/notifications', title: 'Notifications', icon: 'ui-1_bell-53', class: '' },
+
+    { path: '/user-profile', title: 'User Profile', icon: 'users_single-02', class: '' },
+    { path: '/table-list', title: 'Table List', icon: 'design_bullet-list-67', class: '' },
+    { path: '/typography', title: 'Typography', icon: 'text_caps-small', class: '' }
+]
+
+const studentRoutes = [
+    { path: '/home', title: 'Home', icon: 'design_app', class: '' },
+    { path: '/icons', title: 'Icons', icon: 'education_atom', class: '' },
+    { path: '/notifications', title: 'Notifications', icon: 'ui-1_bell-53', class: '' },
+]
+
+const lecturerRoutes = [
+    { path: '/home', title: 'Home', icon: 'design_app', class: '' },
+    { path: '/user-profile', title: 'User Profile', icon: 'users_single-02', class: '' },
+    { path: '/table-list', title: 'Table List', icon: 'design_bullet-list-67', class: '' },
+    { path: '/typography', title: 'Typography', icon: 'text_caps-small', class: '' }
+]
+
+let _routes;
+
+let userType = 'LECTURER';
+
+switch (userType) {
+    case 'ADMIN':
+        _routes = adminRoutes;
+        break;
+    case 'STUDENT':
+        _routes = studentRoutes;
+        break;
+    case 'LECTURER':
+        _routes = lecturerRoutes;
+        break;
+}
+
+export const ROUTES: RouteInfo[] = _routes;
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+    menuItems: any[];
+
+    constructor() { }
+
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+    isMobileMenu() {
+        if (window.innerWidth > 991) {
+            return false;
+        }
+        return true;
+    };
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { TypographyComponent } from '../../typography/typography.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'home',           component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
