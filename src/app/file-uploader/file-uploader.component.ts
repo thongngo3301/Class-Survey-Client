c7151828efa5d7cff68c25130b9fda993dec5bc9
ThongND <thongngo3301@gmail.com>
2018-12-06 15:58:51 +0700

    update file-upload

--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FileError, NgxfUploaderService, UploadEvent, UploadStatus } from 'ngxf-uploader';
+import { FileUploader } from 'ng2-file-upload';
 
-import { ApiService } from './../services/api.service';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'app-file-uploader',
@@ -9,94 +9,10 @@ import { ApiService } from './../services/api.service';
   styleUrls: ['./file-uploader.component.scss']
 })
 export class FileUploaderComponent {
+  public uploader: FileUploader = new FileUploader({ url: URL });
+  public hasBaseDropZoneOver: boolean = false;
 
-  progress = 0;
-  isUploading = false;
-
-  constructor(private Upload: NgxfUploaderService, private apiService: ApiService) { }
-
-  uploadFiles(files: File[] | FileError): void {
-    console.log('selected files', files);
-    this.isUploading = true;
-    if (!(files instanceof Array)) {
-      this.alertError(files);
-      this.isUploading = false;
-      return;
-    }
-    this.apiService.uploadFiles(files).subscribe(res => {
-      console.log(res);
-    });
-    // this.apiService.uploadFiles(files).subscribe(
-    //   (event: UploadEvent) => {
-    //     console.log('upload evt', event);
-    //     this.progress = event.percent;
-    //     if (event.status === UploadStatus.Completed) {
-    //       console.info(`This file upload success! File name: ${event.data.obj.file.originalname}`);
-    //     }
-    //   },
-    //   (err) => {
-    //     console.error(err);
-    //   },
-    //   () => {
-    //     this.isUploading = false;
-    //     this.progress = 0;
-    //     console.log('complete');
-    //   }
-    // );
-  }
-
-  uploadFiles1(files: File[] | FileError): void {
-    console.log('selected files', files);
-    this.isUploading = true;
-    if (!(files instanceof Array)) {
-      this.alertError(files);
-      this.isUploading = false;
-      return;
-    }
-    this.Upload.upload({
-      url: 'https://us-central1-alanzouhome.cloudfunctions.net/api/file',
-      headers: {
-        Authorization: 'token'
-      }, // Option
-      params: {
-        test: '123'
-      }, // Option
-      fields: { // Option
-        toUrl: 'device'
-      },
-      filesKey: 'fileKey', // Option
-      files: files,
-      process: true
-    }).subscribe(
-      (event: UploadEvent) => {
-        console.log('upload evt', event);
-        this.progress = event.percent;
-        if (event.status === UploadStatus.Completed) {
-          console.info(`This file upload success! File name: ${event.data.obj.file.originalname}`);
-        }
-      },
-      (err) => {
-        console.error(err);
-      },
-      () => {
-        this.isUploading = false;
-        this.progress = 0;
-        console.log('complete');
-      });
-  }
-
-  // Do something you want when file error occur.
-  alertError(msg: FileError) {
-    switch (msg) {
-      case FileError.NumError:
-        alert('Number Error');
-        break;
-      case FileError.SizeError:
-        alert('Size Error');
-        break;
-      case FileError.TypeError:
-        alert('Type Error');
-        break;
-    }
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
   }
 }
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,8 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ChartsModule } from 'ng2-charts';
 import { NgxfUploaderModule } from 'ngxf-uploader';
+import { FileUploadModule } from 'ng2-file-upload';
 import { ComponentsModule } from '../../components/components.module';
 import { ModalsModule } from '../../modals/modals.module';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
@@ -28,6 +30,8 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
     ChartsModule,
     HttpClientModule,
     NgxfUploaderModule,
+    ProgressbarModule.forRoot(),
+    FileUploadModule,
     ComponentsModule,
     ModalsModule
   ],
