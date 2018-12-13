9691fcc51b359159b110fc3d26828c014d3b0bb8
ThongND <thongngo3301@gmail.com>
2018-12-13 18:39:55 +0700

    update

--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader, FileUploaderOptions } from 'ng2-file-upload';
+import { ToastrNotificationService } from '../services/toastr-notification.service';
 
 const URL = 'http://192.168.16.158:5000/admins/students';
 
@@ -9,6 +10,8 @@ const URL = 'http://192.168.16.158:5000/admins/students';
   styleUrls: ['./file-uploader.component.scss']
 })
 export class FileUploaderComponent implements OnInit {
+  constructor(private toastr: ToastrNotificationService) { }
+
   public uploader: FileUploader = new FileUploader({ url: URL });
   public uploaderOptions: FileUploaderOptions = {};
   public hasBaseDropZoneOver: boolean = false;
@@ -20,6 +23,14 @@ export class FileUploaderComponent implements OnInit {
     //   form.append('xlsx', item._file);
     // }
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item, res) => {
+      let _res = JSON.parse(res);
+      if (_res.success) {
+        this.toastr.success(`File ${item._file.name} is uploaded successfully`);
+      } else {
+        this.toastr.error(`File ${item._file.name} doesn't have correct format`);
+      }
+    }
   }
 
   public fileOverBase(e: any): void {
